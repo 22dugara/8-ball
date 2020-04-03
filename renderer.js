4a787073e3fafef34743858eaffe9dd1c5ebffc3
Aarav Dugar <aaravdugar@Aaravs-Air.fios-router.home>
2020-04-03 19:21:49 -0400

    50th times the charm

---
+++ renderer.js
@@ -0,0 +1,38 @@
+const electron = require('electron');
+const desktopCapturer = electron.desktopCapturer;
+const electronScreen = electron.remote.screen;
+const shell = electron.shell;
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+var Ctime = process.hrtime();
+var Otime = process.hrtime();
+const thumbSize = determineScreenShotSize();
+let options = {types: ['screen'], thumbnailSize: thumbSize};
+console.log("before get sources");
+desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+    console.log("after get sources");
+ 
+
+    sources.forEach(function(source){
+        if(source.name === 'Entire Screen' || source.name === 'Screen 1'){
+            const screenshotPath = path.join(os.tmpdir(), 'screenshot.png');
+            fs.writeFile(screenshotPath, source.thumbnail.toPNG(), function(err){
+                if(err) return console.log(err.message);
+                console.log('file://' + screenshotPath);
+                shell.openExternal('file://' + screenshotPath);
+                
+            })
+        }
+    })
+})
+
+function determineScreenShotSize(){
+    const screenSize = electronScreen.getPrimaryDisplay().workAreaSize;
+    const maxDimension = Math.max(screenSize.width, screenSize.height);
+    return{
+        width: maxDimension * window.devicePixelRatio,
+        height: maxDimension * window.devicePixelRatio,
+    };
+}
