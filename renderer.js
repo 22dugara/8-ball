205567c7a7058a748c4ceda65f2ec714e5c40f8c
Aarav Dugar <aaravdugar@Aaravs-Air.fios-router.home>
2020-04-05 22:55:25 -0400

    displays images

--- renderer.js
+++ renderer.js
@@ -6,29 +6,42 @@ const shell = electron.shell;
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-var Ctime = process.hrtime();
-var Otime = process.hrtime();
+var number  = 0;
+
 const thumbSize = determineScreenShotSize();
 let options = {types: ['screen'], thumbnailSize: thumbSize};
 console.log("before get sources");
-desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+console.log(process.hrtime());
+var Ctime = process.hrtime()[0];
+var Otime = process.hrtime()[0];
+
+
+function getball(){
+desktopCapturer.getSources({ types: ['window', 'screen'], thumbnailSize: {width: 500 , height: 500} }).then(async sources => {
     console.log("after get sources");
  
 
     sources.forEach(function(source){
         console.log(source.name);
        if(source.name === '8 Ball Pool - A free Sports Game'){
-            const screenshotPath = path.join(os.tmpdir(), 'screenshot.png');
+           const ScreenpathString = 'screenshot' + number + '.png';
+            const screenshotPath = path.join(os.tmpdir(), ScreenpathString);
             fs.writeFile(screenshotPath, source.thumbnail.toPNG(), function(err){
                 if(err) return console.log(err.message);
                 console.log('file://' + screenshotPath);
-                shell.openExternal('file://' + screenshotPath);
+                document.getElementById("8-ball").src = 'file://' + screenshotPath;
+
                 
             })
         }
     })
-})
 
+    console.log(process.hrtime());
+    number = number + 1;
+    setTimeout(getball, 100);
+})
+}
+getball();
 function determineScreenShotSize(){
     const screenSize = electronScreen.getPrimaryDisplay().workAreaSize;
     const maxDimension = Math.max(screenSize.width, screenSize.height);
